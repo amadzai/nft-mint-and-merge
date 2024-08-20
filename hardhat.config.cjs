a4efe7d33e7ba2c55ac3fd5cdf5d55abbea265c8
Amad Zai <20966134+amadzai@users.noreply.github.com>
2024-08-20 22:52:36 +0800

    Increase gas and block gas limit

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -5,5 +5,15 @@ module.exports = {
   solidity: "0.8.20",
   paths: {
     artifacts: './src/artifacts',
-  }
+  },
+  networks: {
+    hardhat: {
+      gas: 12000000, 
+      blockGasLimit: 0x1fffffffffffff,
+    //   mining: {
+    //     auto: false,
+    //     interval: 5000 
+    //   },
+    },
+  },
 };
