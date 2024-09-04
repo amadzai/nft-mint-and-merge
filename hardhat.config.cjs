3a23f8d393941f41af8747bb73cfa0b75491be6c
Amad Zai <20966134+amadzai@users.noreply.github.com>
2024-09-04 15:42:09 +0800

    Remove duplicate import

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -1,5 +1,4 @@
 require("@nomiclabs/hardhat-waffle");
-require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-ethers");
 require("@nomiclabs/hardhat-etherscan");
 require('dotenv').config();  // To load environment variables from .env
